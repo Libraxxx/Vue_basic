8033af5b3d0ab1b434e67b74ba58462c06166933
Libra <1679448145@qq.com>
2021-07-07 15:38:36 +0800

    更新

---
+++ 19-组件_单文件/APP.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Hello/>
+  </div>
+</template>
+<script>
+import Hello from '19-组件_单文件/components/Hello'
+export default {
+  components:{
+    Hello
+  }
+}
+</script>
+<style>
+  
+</style>
---
+++ 19-组件_单文件/components/Hello.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2 class="title">名字：{{name}}</h2>
+    <h2 @click="show">地址：{{addr}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  
+  data() {
+    return {
+      name: "ajdaj",
+      addr: "456"
+    }
+  },
+  methods: {
+    show(){
+      alert(this.addr)
+    }
+  },
+}
+</script>
+
+<style>
+  .title{
+    background-color: pink;
+  }
+</style>
